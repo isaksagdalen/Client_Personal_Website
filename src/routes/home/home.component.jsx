467d75fa2ab4eb6808db43fec180c56731240f76
isaksagdalen <Isakhsag@outlook.com>
2022-11-18 16:30:41 +0100

    fak sass

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,17 @@
-import "./home.styles.scss";
+import "./home.styles.css";
 
-function Home() {}
+function Home() {
+  <header className="header">
+    <navigation className="nav">
+      <div className="logo-box">
+        <img
+          src={require("../../assets/original.png")}
+          alt="logo"
+          className="logo"
+        />
+      </div>
+    </navigation>
+  </header>;
+}
 
 export default Home;
