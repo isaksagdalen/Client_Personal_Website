fc636f442b8903815235e3e6a0756b59fdf5a8e7
Andre Skjelbred <skjelbred321@gmail.com>
2022-11-18 16:26:38 +0100

    HH

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./routes/home/home.component";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
