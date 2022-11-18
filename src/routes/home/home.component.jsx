ef4a15b2b7f3cba8baf2061eabdc1f6ac5014bce
Andre Skjelbred <skjelbred321@gmail.com>
2022-11-18 16:33:41 +0100

    IJ

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,17 +1,20 @@
 import "./home.styles.css";
 
 function Home() {
-  <header className="header">
-    <navigation className="nav">
-      <div className="logo-box">
-        <img
-          src={require("../../assets/original.png")}
-          alt="logo"
-          className="logo"
-        />
-      </div>
-    </navigation>
-  </header>;
+  return (
+    <header className='header'>
+      Nigger
+      <navigation className='nav'>
+        <div className='logo-box'>
+          <img
+            src={require("../../assets/original.png")}
+            alt='logo'
+            className='logo'
+          />
+        </div>
+      </navigation>
+    </header>
+  );
 }
 
 export default Home;
