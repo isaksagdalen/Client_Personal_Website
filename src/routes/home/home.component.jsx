939c23009a9dd2206c54d6676562913e12c94385
Andre Skjelbred <skjelbred321@gmail.com>
2022-11-20 12:40:04 +0100

    IHOH

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -84,8 +84,8 @@ function Home() {
         </div>
       </section>
 
-      <section id='2' className='priser'>
-        <div className='container grid grid--3-cols align-center-v'>
+      <section className='priser'>
+        <div id='2' className='container grid grid--3-cols align-center-v'>
           <div className='card'>
             <div className='card-side card-side-front'>
               <div className='card-picture card-picture-1'>&nbsp;</div>
