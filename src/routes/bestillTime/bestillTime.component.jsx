9a70db47eb4f24c8781de43b1ef230f2733d6be1
isaksagdalen <Isakhsag@outlook.com>
2023-01-11 18:17:21 +0100

    Final touches

--- src/routes/bestillTime/bestillTime.component.jsx
+++ src/routes/bestillTime/bestillTime.component.jsx
@@ -4,34 +4,12 @@ import Footer from "../../components/footer/footer.component";
 import Menu from "../../components/menu/menu.component";
 import FieldContainer from "../../components/fieldContainer/fieldContainer.component";
 import React, { useRef } from "react";
-import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
+
 import emailjs from "@emailjs/browser";
 import { useState, useEffect } from "react";
 
 function BestillTime() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng] = useState(10.3975004);
-  const [lat] = useState(63.4340235);
-  const [zoom] = useState(15);
-  const [API_KEY] = useState("tLpT5Ko2C8ATzfO1hfvV");
-
-  useEffect(() => {
-    if (map.current) return; //stops map from intializing more than once
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
-      center: [lng, lat],
-      zoom: zoom,
-    });
-    map.current.addControl(new maplibregl.NavigationControl(), "top-right");
-    new maplibregl.Marker({ color: "#FF0000" })
-      .setLngLat([lng, lat])
-      .addTo(map.current);
-  });
 
   const form = useRef();
   const sendEmail = () => {
@@ -54,43 +32,52 @@ function BestillTime() {
       <NavBar bestill={true} />
       <Menu />
       {isModalOpen && (
-        <div className='modal hidden'>
-          <h2 className='mld'>Melding Sendt!</h2>
-          <button onClick={toggleModal} className='close-modal'>
+        <div className="modal hidden">
+          <h2 className="mld">Melding Sendt!</h2>
+          <button onClick={toggleModal} className="close-modal">
             &times;
           </button>
         </div>
       )}
-      <div className='map-form-container'>
-        <div className='map-title-container'>
-          <h2 className='map-title'>Adresse: Nordre Gate 21</h2>
-          <div className='map-wrap'>
-            <div ref={mapContainer} className='map' />
+      <div className="map-form-container">
+        <div className="map-title-container">
+          <h2 className="map-title">Adresse: Nordre Gate 21</h2>
+          <div className="map-wrap">
+            <a
+              href="https://www.google.com/maps/@63.4340235,10.3975004,19z"
+              target="_blank"
+            >
+              <img
+                alt="map"
+                src={require("../../assets/map.png")}
+                className="map"
+              />
+            </a>
           </div>
         </div>
 
-        <form ref={form} className='form-fields' onSubmit={sendEmail}>
+        <form ref={form} className="form-fields" onSubmit={sendEmail}>
           <FieldContainer
-            name='user_name'
-            type='text'
-            title='Navn'
-            placeholder='Anna Skjelbred'
+            name="user_name"
+            type="text"
+            title="Navn"
+            placeholder="Anna Skjelbred"
           />
           <FieldContainer
-            name='user_email'
-            type='email'
+            name="user_email"
+            type="email"
             required
-            title='E-postadresse'
-            placeholder='anna@gmail.com'
+            title="E-postadresse"
+            placeholder="anna@gmail.com"
           />
           <FieldContainer
-            name='message'
+            name="message"
             large={true}
-            type='text'
-            title='Melding'
-            placeholder=''
+            type="text"
+            title="Melding"
+            placeholder=""
           />
-          <button className='submit-form'>Send</button>
+          <button className="submit-form">Send</button>
         </form>
       </div>
 
