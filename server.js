289c3de626a56d68ac727c58da10050ecb893e3b
Andre Skjelbred <skjelbred321@gmail.com>
2023-01-29 16:47:50 +0100

    M

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const prerender = require("prerender-node");
+const path = require("path");
+const app = express();
+
+prerender.set("prerenderToken", "TFBiRy7DjbfcgJQ4Yb0c");
+
+app.use(prerender);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
